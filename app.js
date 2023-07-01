d610530639aebb14319ab5c899d81cbc51bf24b8
Michelle Hoffmann <michelley9v@yahoo.com>
2023-06-30 20:56:26 -0500

    First Ex

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+function  plus(num) {
+return function (plusNumber) {
+  return num + plusNumber;
+  }
+}
+const plus2 = plus(2);
+
+console.log(plus2(2)); 
+console.log(plus2(5));
+
+const plus100 = plus(100);
+
+console.log(plus100(100)); 
+console.log(plus100(5));
+
+//Exercise 2 Section
+
